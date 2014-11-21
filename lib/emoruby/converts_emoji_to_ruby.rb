25659c9c659b1e0c8cca095a6c92f411ca609c13
Justin Searls <searls@gmail.com>
2014-11-21 13:43:28 -0500

    refactoring

--- lib/emoruby/converts_emoji_to_ruby.rb
+++ lib/emoruby/converts_emoji_to_ruby.rb
@@ -19,13 +19,31 @@ class ConvertsEmojiToRuby
   ]
 
   def call(source)
-    source.chars.map do |char|
-      if emoji = EmojiData.find_by_str(char).first
-        TRANSLATIONS[emoji.short_name] || emoji.short_name
-      elsif GARBAGE.include?(char.bytes)
-        ""
+    translate_chars(clean_chars(source.chars)) do |char, previous_translation|
+      if emoji_name = emoji_name_for(char)
+        emoji_name
+      end
+    end
+  end
+
+private
+
+  def emoji_name_for(char)
+    return unless emoji = EmojiData.find_by_str(char).first
+    TRANSLATIONS[emoji.short_name] || emoji.short_name
+  end
+
+  def clean_chars(source)
+    source.reject {|c| GARBAGE.include?(c.bytes) }
+  end
+
+  def translate_chars(chars)
+    previous_translation = nil
+    chars.each_with_index.map do |original_char, i|
+      previous_translation = if new_char = yield(original_char, previous_translation, i)
+        new_char
       else
-        char
+        original_char
       end
     end.join
   end
