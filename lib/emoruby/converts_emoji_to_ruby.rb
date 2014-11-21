56adefbafb1d525367333fa5816de4847d5dee1b
Justin Searls <searls@gmail.com>
2014-11-21 14:17:20 -0500

    do actual constant conversion. Whew.

--- lib/emoruby/converts_emoji_to_ruby.rb
+++ lib/emoruby/converts_emoji_to_ruby.rb
@@ -1,50 +1,58 @@
 require 'emoji_data'
+require 'emoruby/util/string'
 
-class ConvertsEmojiToRuby
-  TRANSLATIONS = {
-    "clipboard" => "class",
-    "soon" => "def",
-    "eyes" => "puts",
-    "speech_balloon" => "\"",
-    # "end" => "end", #<- redundant!
-    "black_small_square" => ".",
-    "hatching_chick" => "new",
+module Emoruby
+  class ConvertsEmojiToRuby
+    TRANSLATIONS = {
+      "clipboard" => "class",
+      "soon" => "def",
+      "eyes" => "puts",
+      "speech_balloon" => "\"",
+      "black_small_square" => ".",
+      "hatching_chick" => "new"
+    }
 
-    # cheats until i can translate names
-    "heart" => "Heart"
-  }
+    GARBAGE = [
+      [239,184,143] # evil problematic whitespace where is it from where am i what even
+    ]
 
-  GARBAGE = [
-    [239,184,143] # evil problematic whitespace where is it from where am i what even
-  ]
+    def call(source)
+      chars = clean_chars(scan_and_translate_constants(source).chars)
 
-  def call(source)
-    translate_chars(clean_chars(source.chars)) do |char, previous_translation|
-      if emoji_name = emoji_name_for(char)
+      translate_chars(chars) do |char|
+        next unless emoji_name = emoji_name_for(char)
         emoji_name
       end
     end
-  end
 
-private
+  private
 
-  def emoji_name_for(char)
-    return unless emoji = EmojiData.find_by_str(char).first
-    TRANSLATIONS[emoji.short_name] || emoji.short_name
-  end
+    def scan_and_translate_constants(source)
+      constant_map = Hash[source.split(/\s+/).each_cons(2).map do |operator, emo_constant|
+        next unless ["class", "module"].include?(emoji_name_for(operator))
+        [emo_constant, Util::String.classify(emo_constant.chars.map {|c| emoji_name_for(c) }.join)]
+      end.compact]
 
-  def clean_chars(source)
-    source.reject {|c| GARBAGE.include?(c.bytes) }
-  end
+      source.gsub(Regexp.new(constant_map.keys.map { |x| Regexp.escape(x) }.join('|')), constant_map)
+    end
 
-  def translate_chars(chars)
-    previous_translation = nil
-    chars.each_with_index.map do |original_char, i|
-      previous_translation = if new_char = yield(original_char, previous_translation, i)
-        new_char
-      else
-        original_char
-      end
-    end.join
+    def emoji_name_for(char)
+      return unless emoji = EmojiData.find_by_str(char).first
+      TRANSLATIONS[emoji.short_name] || emoji.short_name
+    end
+
+    def clean_chars(source)
+      source.reject {|c| GARBAGE.include?(c.bytes) }
+    end
+
+    def translate_chars(chars)
+      chars.each_with_index.map do |original_char, i|
+        if new_char = yield(original_char, i)
+          new_char
+        else
+          original_char
+        end
+      end.join
+    end
   end
 end
---
+++ lib/emoruby/util/string.rb
@@ -0,0 +1,22 @@
+# Ripped pretty much right out of activesupport 4.1.8
+# activesupport/lib/active_support/inflector/methods.rb
+
+module Emoruby
+  module Util
+    module String
+      def self.classify(string)
+        camelize(string.sub(/.*\./, ''))
+      end
+
+    private
+
+      def self.camelize(term)
+        string = term.to_s
+        string = string.sub(/^[a-z\d]*/) { $&.capitalize }
+        string.gsub!(/(?:_|(\/))([a-z\d]*)/i) { "#{$1}#{$2.capitalize}" }
+        string.gsub!('/', '::')
+        string
+      end
+    end
+  end
+end
