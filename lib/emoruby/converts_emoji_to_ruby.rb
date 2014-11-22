51fbaac2082e6828a767b48e36be9dbb6a205f87
Justin Searls <searls@gmail.com>
2014-11-22 10:11:16 -0500

    move translations into a YAML file

--- lib/emoruby/converts_emoji_to_ruby.rb
+++ lib/emoruby/converts_emoji_to_ruby.rb
@@ -1,17 +1,12 @@
 require 'emoji_data'
 require 'emoruby/util/string'
 
+require 'yaml'
+require 'pathname'
+
 module Emoruby
   class ConvertsEmojiToRuby
-    TRANSLATIONS = {
-      "clipboard" => "class",
-      "soon" => "def",
-      "eyes" => "puts",
-      "speech_balloon" => "\"",
-      "black_small_square" => ".",
-      "hatching_chick" => "new",
-      "boom" => "raise"
-    }
+    TRANSLATIONS = YAML.load(File.read(Pathname.new(File.dirname(__FILE__)).join("..","..","config","translations.yml")))
 
     GARBAGE = [
       [239,184,143] # evil problematic whitespace where is it from where am i what even
@@ -39,7 +34,7 @@ module Emoruby
 
     def emoji_name_for(char)
       return unless emoji = EmojiData.find_by_str(char).first
-      TRANSLATIONS[emoji.short_name] || emoji.short_name
+      TRANSLATIONS[emoji.to_s] || emoji.short_name
     end
 
     def clean_chars(source)
