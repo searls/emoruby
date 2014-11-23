471a90f35e7bbad38fd1c86d48bbc080e11e4bdc
Philip Arndt <git@arndt.io>
2014-11-22 17:35:41 -0700

    Prevent issue where a blank expected file passed.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,13 +12,13 @@ end
 
 RSpec::Matchers.define :match_all_the_characters_of do |expected|
   match do |actual|
-    expected.chars.each_with_index.all? do |c, i|
-      actual[i] == c
+    actual.chars.each_with_index.all? do |c, i|
+      expected[i] == c
     end
   end
 
   failure_message_for_should do |actual|
-    expected.chars.each_with_index.reject { |c, i| actual[i] == c }.map do |c,i|
+    actual.chars.each_with_index.reject { |c, i| expected[i] == c }.map do |c,i|
       "expected that character #{i} would be #{c} but was #{actual[i]} (near '#{actual[i-5..i+5]}')"
     end.join("\n")
   end
