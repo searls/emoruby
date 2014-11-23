ed880ff3c5a4413370656326f86639d7b2c62f5d
Justin Searls <searls@gmail.com>
2014-11-23 12:23:07 -0500

    Moved the source comparison to an invariant
    in rspec-given, Invariants will always run before every downstream test

--- spec/1_hello_world_spec.rb
+++ spec/1_hello_world_spec.rb
@@ -3,16 +3,11 @@ require 'spec_helper'
 require 'emoruby'
 
 describe Emoruby do
-  Given(:emo_source_with_stripped_puts) { emo_source.gsub(EmojiData.find_by_short_name("eyes").first.to_s, "") }
+  Invariant { Emoruby.emoji_to_ruby(emo_source) == expected_ruby }
 
   context "a hello world app" do
     Given(:emo_source) { load_fixture("1_hello_world") }
     Given(:expected_ruby) { load_fixture("1_hello_world","rb") }
-
-    describe 'translating source' do
-      When(:result) { Emoruby.emoji_to_ruby(emo_source) }
-      Then { result == expected_ruby }
-    end
 
     describe 'evaluating source' do
       When(:result) { Emoruby.eval(emo_source_with_stripped_puts) }
@@ -20,14 +15,9 @@ describe Emoruby do
     end
   end
 
-  context "a hello world app with a private method" do
+  context "a hello world app with a private & protected methods" do
     Given(:emo_source) { load_fixture("4_method_access") }
     Given(:expected_ruby) { load_fixture("4_method_access", "rb")}
-
-    describe 'translating source' do
-      When(:result) { Emoruby.emoji_to_ruby(emo_source) }
-      Then { result == expected_ruby }
-    end
 
     describe 'evaluating source' do
       When(:result) { Emoruby.eval(emo_source_with_stripped_puts) }
@@ -37,4 +27,6 @@ describe Emoruby do
       And { expect(Snowman).to be_private_method_defined(:wave) }
     end
   end
+
+  Given(:emo_source_with_stripped_puts) { emo_source.gsub(EmojiData.find_by_short_name("eyes").first.to_s, "") }
 end
