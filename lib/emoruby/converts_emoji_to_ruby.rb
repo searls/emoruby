efb505c6d35a2f3066194f481b82655502582efb
Justin Searls <searls@gmail.com>
2014-11-23 12:55:14 -0500

    reorder functions to invocation-order

--- lib/emoruby/converts_emoji_to_ruby.rb
+++ lib/emoruby/converts_emoji_to_ruby.rb
@@ -19,36 +19,6 @@ module Emoruby
 
   private
     attr_reader :source
-    def constant_map
-      @constant_map ||= Hash[
-        source.split(/\s+/).each_cons(2).map do |operator, emo_constant|
-          next unless ['class', 'module'].include?(emoji_name_for(operator))
-          [
-            emo_constant,
-            Util::String.classify(emo_constant.chars.map {|c| emoji_name_for(c) }.join)
-          ]
-        end.compact
-      ]
-    end
-
-    def scan_and_translate_constants(source)
-      source.gsub(Regexp.new(constant_map.keys.map { |x| Regexp.escape(x) }.join('|')), constant_map)
-    end
-
-    def emoji_name_for(char)
-      return unless emoji = EmojiData.find_by_str(char).first
-      TRANSLATIONS[emoji.to_s] || emoji.short_name
-    end
-
-    def clean_chars(source)
-      source.reject {|c| GARBAGE.include?(c.bytes) }
-    end
-
-    def translate_chars(chars)
-      chars.each_with_index.map do |char, index|
-        yield(char, index) || char
-      end.join
-    end
 
     def translate_lines(lines)
       lines.map do |line|
@@ -62,6 +32,37 @@ module Emoruby
           emo_char
         end
       end
+    end
+
+    def scan_and_translate_constants(source)
+      source.gsub(Regexp.new(constant_map.keys.map { |x| Regexp.escape(x) }.join('|')), constant_map)
+    end
+
+    def constant_map
+      @constant_map ||= Hash[
+        source.split(/\s+/).each_cons(2).map do |operator, emo_constant|
+          next unless ['class', 'module'].include?(emoji_name_for(operator))
+          [
+            emo_constant,
+            Util::String.classify(emo_constant.chars.map {|c| emoji_name_for(c) }.join)
+          ]
+        end.compact
+      ]
+    end
+
+    def clean_chars(source)
+      source.reject {|c| GARBAGE.include?(c.bytes) }
+    end
+
+    def translate_chars(chars)
+      chars.each_with_index.map do |char, index|
+        yield(char, index) || char
+      end.join
+    end
+
+    def emoji_name_for(char)
+      return unless emoji = EmojiData.find_by_str(char).first
+      TRANSLATIONS[emoji.to_s] || emoji.short_name
     end
   end
 end
