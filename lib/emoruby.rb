56bb0875bcc7c8125ed0ec1e919eb82b80cd0f07
Dmitry Krasnoukhov <dmitry@krasnoukhov.com>
2014-11-25 00:54:01 +0200

    Eval binding improved

--- lib/emoruby.rb
+++ lib/emoruby.rb
@@ -1,18 +1,13 @@
 require "emoruby/version"
 require "emoruby/emoji_script"
 
-# TODO there has to be a better way to eval on top of the world, right?
-def __emoruby_top_level_binding_eval(source)
-  binding.eval(source)
-end
-
 module Emoruby
   def self.emoji_to_ruby(source)
     EmojiScript.new(source).to_ruby
   end
 
   def self.eval(source)
-    __emoruby_top_level_binding_eval(emoji_to_ruby(source))
+    TOPLEVEL_BINDING.eval(emoji_to_ruby(source))
   end
 
   def self.register(file_extension = "emoruby")
