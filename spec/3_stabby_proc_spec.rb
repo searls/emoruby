5fa4a98737884f29ab0e39167fe58c68f3d39cf4
Justin Searls <searls@gmail.com>
2014-11-24 21:09:46 -0500

    style points

--- spec/3_stabby_proc_spec.rb
+++ spec/3_stabby_proc_spec.rb
@@ -5,5 +5,5 @@ require 'emoruby'
 describe Emoruby do
   Given(:source) { load_fixture("3_stabby_proc") }
   When(:result) { Emoruby.eval(source) }
-  Then { expect(result).to eq("smiley") }
+  Then { result == "smiley" }
 end
