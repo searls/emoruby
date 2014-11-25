8dc9a2a79d338f3597bb785689ca07d0c4f71d4b
Justin Searls <searls@gmail.com>
2014-11-24 21:10:23 -0500

    add blocks stuff
    add array, hash/block literals, (snowboard half-)pipe, and map (pin).

---
+++ spec/6_blocks_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+require 'emoruby'
+
+describe Emoruby do
+  Given(:source) { load_fixture("6_blocks") }
+  When(:result) { Emoruby.eval(source) }
+  Then { result == [10, 4] }
+end
