1e123f4539523c86cdd6bfd686dc2f11e4592b95
Philip Arndt <git@p.arndt.io>
2015-06-03 21:20:19 +1200

    Added Emoruby.eval_to_emoji & Emoruby.capture_eval
    Emoruby.eval_to_emoji("💭 Hello, world! 👋
    👉 🔨
     💬😃💬
     🔚▪️📞")
    
    => "😃"
    
    Emoruby.capture_eval("puts 'smiley'")
    
    => "\"smiley\""
    
    This allows us to capture the output even when
    'puts' is the final statement, e.g. 👀💬😃 🌏💬

--- lib/emoruby.rb
+++ lib/emoruby.rb
@@ -1,6 +1,7 @@
 require "emoruby/version"
 require "emoruby/emoji_script"
 require "emoruby/converts_ruby_to_emoji"
+require "stringio"
 
 module Emoruby
   def self.emoji_to_ruby(source)
@@ -11,8 +12,22 @@ module Emoruby
     ConvertsRubyToEmoji.new(source).call
   end
 
+  def self.capture_eval(source)
+    begin
+      $stdout = StringIO.new
+      eval(source)
+      $stdout.string.rstrip.inspect
+    ensure
+      $stdout = STDOUT
+    end
+  end
+
   def self.eval(source)
     TOPLEVEL_BINDING.eval(emoji_to_ruby(source))
+  end
+
+  def self.eval_to_emoji(source)
+    ruby_to_emoji(eval(source))
   end
 
   def self.register(file_extension = "emoruby")
